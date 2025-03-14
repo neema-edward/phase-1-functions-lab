f65b1544559b4e4192ee33d677669348c988a1bf
Neema <neema.edward@student.moringaschool.com>
2025-03-14 10:09:35 +0300

    commit assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (block) {
+    return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet (block) {
+    return Math.abs(block - 42) * 264;
+}
+
+function distanceTravelledInFeet ( start, destination ) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice (start, destination) {
+    let distance = Math.abs(start - destination) * 264;
+    console.log("Free sample", distance);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+    
+}
